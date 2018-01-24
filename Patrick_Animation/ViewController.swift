9c6474f07dc7d6368e9de41e368b7c6cf782c9f8
Pratik <pratikpanchal@pratik-imac.local>
2018-01-23 23:03:47 -0800

    Initial Commit

---
+++ Patrick_Animation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Patrick_Animation
+//
+//  Created by pratik panchal on 23/01/18.
+//  Copyright © 2018 com.Panamax.PushTestApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
